a47022f58ae9850d48883c44eec45d5061f49ffe
Emily Rhodes <erhodes.345@gmail.com>
2024-09-05 17:02:59 +0100

    initial commit

---
+++ src/main/java/com/ncrecordshop/recordshopspringbootapp/RecordshopspringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.ncrecordshop.recordshopspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordshopspringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordshopspringbootappApplication.class, args);
+	}
+
+}
