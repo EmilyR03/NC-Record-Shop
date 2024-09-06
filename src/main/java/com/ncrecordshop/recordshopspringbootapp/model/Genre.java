b6c5b605c2aaa1287b5c50a35f977741aaf24932
Emily Rhodes <erhodes.345@gmail.com>
2024-09-06 10:31:30 +0100

    feat: add entity fields to Record Class

--- src/main/java/com/ncrecordshop/recordshopspringbootapp/model/Genre.java
+++ src/main/java/com/ncrecordshop/recordshopspringbootapp/model/Genre.java
@@ -1,4 +1,15 @@
 package com.ncrecordshop.recordshopspringbootapp.model;
 
 public enum Genre {
+
+        ROCK("Rock"),
+        POP("Pop"),
+        Jazz ("Jazz"),
+        HipHop ("HipHop");
+
+        // public final String description;
+
+        Genre(String description){}
+    }
+
 }
