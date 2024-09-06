8b7590257f8630b3d3e7da329e02e4dd321f95f7
Emily Rhodes <erhodes.345@gmail.com>
2024-09-06 10:33:15 +0100

    feat:  add the enums to Genre Enum  add entity fields to record class

--- src/main/java/com/ncrecordshop/recordshopspringbootapp/model/Record.java
+++ src/main/java/com/ncrecordshop/recordshopspringbootapp/model/Record.java
@@ -1,4 +1,35 @@
 package com.ncrecordshop.recordshopspringbootapp.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="Records")
+
 public class Record {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    Long id;
+
+    @Column
+    String artist;
+
+    @Column
+    String album;
+
+    @Column
+    int releaseDate;
+
+    @Column
+    Genre genre;
+
 }
